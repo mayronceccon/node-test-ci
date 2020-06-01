950b10ca9afa4525dbc72c2dbf44a06335585e51
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-06-01 20:59:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const sum = require("./sum");
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ name: "Test", sum: sum(10, 15) });
+});
+
+app.listen((port = PORT), () => {
+  console.log("Run server");
+});
